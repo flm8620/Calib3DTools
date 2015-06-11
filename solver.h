84ba431a8aabf9262b9247f507a425f0fd3ef3cc
flm8620 <flm8620@gmail.com>
2015-06-11 21:39:30 +0200

    cmake

--- solver.h
+++ solver.h
@@ -1,48 +1,7 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
-<<<<<<< HEAD
-#include <QObject>
-#include <QList>
-#include <QPointF>
-#include <QVector3D>
-class QImage;
-class ImageListModel;
-class DistortionModel;
-class KMatrixModel;
-class Point2DModel;
-class Point3DModel;
-class KMatrix{
-public:
-    double fx,fy,x0,y0,s;
-};
-Q_DECLARE_METATYPE(KMatrix)
-class Distortion{
-public:
-    QList<double> data;
-    bool isEmpty(){return data.size()==0;}
-    void makeEmpty(){data.clear();}
-};
-Q_DECLARE_METATYPE(Distortion)
-class Target2D{
-public:
-    QList<QList<QPointF> > data;
-    bool isEmpty(){return data.size()==0;}
-    void makeEmpty(){data.clear();}
-};
-Q_DECLARE_METATYPE(Target2D)
-class Target3D{
-public:
-    QList<QVector3D> data;
-    bool isEmpty(){return data.size()==0;}
-    void makeEmpty(){data.clear();}
-};
-Q_DECLARE_METATYPE(Target3D)
-class CameraPosSolution{
-public:
-    QList<QVector3D> data;
-};
-=======
+
 #include "imagelist.h"
 #include "distrortion.h"
 #include "kmatrix.h"
@@ -52,7 +11,7 @@ public:
 
 #include <QtCore>
 #include <QtGui>
->>>>>>> origin/refactor
+
 
 class Solver : public QObject
 {
