33484bf03648b25143c8901a5c72b3fb97c24a25
flm8620 <flm8620@gmail.com>
2015-06-17 11:03:09 +0200

    only for a test

--- main.cpp
+++ main.cpp
@@ -1,24 +1,2 @@
-#include <QApplication>
-#include <QList>
-#include <QImage>
-#include "mainwindow.h"
-#include "distortion.h"
-#include "imagelist.h"
-#include "target2d.h"
-#include "target3d.h"
-#include "kmatrix.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    //qRegisterMetaType<Distortion>();
-    qRegisterMetaType<ImageList >("ImageList");
-    qRegisterMetaType<Target2D>();
-    qRegisterMetaType<Target3D>("Target3D");
-    //qRegisterMetaType<KMatrix>();
-
-    MainWindow w;
-    w.show();
-
-    return a.exec();
-}
+jdsahgfkjahjghakalkf
+etsetests
